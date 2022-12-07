d61a3f6b917d31f8865fa21aa01fa5300d28a0f4
kl0039 <120065556+kl0039@users.noreply.github.com>
2022-12-07 23:10:08 +0000

    First Smart Contract coding

---
+++ Hello.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.13 and less than that also OK
+pragma solidity ^0.8.13;
+
+contract HelloWorld{
+  string public great = "Hello World!";
+ }
